83c7ac70e9d3fddd1552915411170bf1781ac554
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-02 19:07:36 +0530

    setup  for the server

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app =express()
+const port=process.env.PORT || 3000
 console.log(process.env.DB_URL)
 
-app.listen(3000,(req,res)=>{
-  console.log('Server listening at PORT 3000')
+app.listen(port,(req,res)=>{
+  console.log(`Server listening at PORT ${port}`)
 })
