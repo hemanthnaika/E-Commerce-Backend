ccebecf72812efc4a3847ff45d1bd18d360d7c81
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-02 19:24:29 +0530

    To connect to the database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import  express  from "express";
 import dotenv from 'dotenv'
+import connectDB from "./services/mongodb/connectDB";
 dotenv.config()
 
 const app =express()
 const port=process.env.PORT || 3000
-console.log(process.env.DB_URL)
+connectDB()
+// console.log(process.env.DB_URL)
 
 app.listen(port,(req,res)=>{
   console.log(`Server listening at PORT ${port}`)
