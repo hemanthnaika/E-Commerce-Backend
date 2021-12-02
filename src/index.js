0ba17c25eb43db18f33e071c8b41a4fc1869096d
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-02 19:01:24 +0530

    Add the env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import  express  from "express";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app =express()
+console.log(process.env.DB_URL)
+
 app.listen(3000,(req,res)=>{
   console.log('Server listening at PORT 3000')
 })
