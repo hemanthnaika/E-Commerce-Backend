9311a4b42ac84db421991609c308065129abb7c0
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-04 17:18:57 +0530

    Auth And Admin router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import  express  from "express";
 import dotenv from 'dotenv'
 import connectDB from "./services/mongodb/connectDB";
 dotenv.config()
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
 
 const app =express()
 const port=process.env.PORT || 3000
 connectDB()
-// console.log(process.env.DB_URL)
+
+app.use(cors())
+app.use(express.json())
+// !Route to handle auth requests
+app.use("/api/v1/auth",authRoutes)
 
 app.listen(port,(req,res)=>{
   console.log(`Server listening at PORT ${port}`)
