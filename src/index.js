082a1677e416e5ffca28f3f7e7279dfae3beb243
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-08 19:38:23 +0530

    Add the product routes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ dotenv.config()
 import cors from 'cors'
 import authRoutes from './routes/authRoutes'
 import categoryRoutes from './routes/categoryRoutes'
+import productRoutes from './routes/productRoutes'
 
 const app =express()
 const port=process.env.PORT || 3000
@@ -16,6 +17,7 @@ app.use(express.json())
 // !Route to handle auth requests
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/category", categoryRoutes)
+app.use("api/v1/product",productRoutes)
 
 app.listen(port,(req,res)=>{
   console.log(`Server listening at PORT ${port}`)
